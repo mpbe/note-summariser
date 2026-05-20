8e8444ef04dbfa5679827b7158046f4396a302bf
mpbe <mbell85@ymail.com>
2026-05-20 11:32:50 +0100

    message7

--- api/routes.py
+++ api/routes.py
@@ -1,24 +1,23 @@
 from flask import render_template, Blueprint, request, redirect, url_for, flash
-from .services import ai_service
+from api.services import ai_service
 
 main = Blueprint("main", __name__)
 
 query_history = []
 
+
 @main.route("/", methods=["GET", "POST"])
 def index():
-
     response = None
 
     if request.method == "POST":
-
         notes = request.form.get("notes", "").strip()
+
         if not notes:
             flash("please enter text in the box to continue", "error")
             return redirect(url_for("main.index"))
 
         response = ai_service.summarise_clinical_notes(notes=notes)
-
         query_history.append(response)
 
     return render_template("index.html", response=response)
